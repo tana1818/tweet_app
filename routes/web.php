b01db8ee9fffa46909147ff25548183d0cc340e7
tana1818 <tanakagen1124@gmail.com>
2019-07-23 16:35:01 +0900

    add_to_post_routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PostsController@index')->name('top');
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
